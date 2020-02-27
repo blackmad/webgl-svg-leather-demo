4211129602d1025e3fe01b70657fa45065e8cdfe
David Blackman <david@whizziwig.com>
2020-02-26 22:59:45 -0500

    that worked bizarrely well

--- main.js
+++ main.js
@@ -6,32 +6,30 @@ animate();
 //
 
 function init() {
-
-  var container = document.getElementById( 'container' );
-
-  //
-
-  camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 1000 );
-  camera.position.set( 0, 0, 200 );
+  var container = document.getElementById("container");
 
   //
 
-  renderer = new THREE.WebGLRenderer( { antialias: true } );
-  renderer.setPixelRatio( window.devicePixelRatio );
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  container.appendChild( renderer.domElement );
+  camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 1000);
+  camera.position.set(0, 0, 200);
 
   //
 
-  var controls = new THREE.OrbitControls( camera, renderer.domElement );
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  container.appendChild(renderer.domElement);
+
+  //
+
+  var controls = new THREE.OrbitControls(camera, renderer.domElement);
   controls.screenSpacePanning = true;
 
   //
 
   //
 
-  window.addEventListener( 'resize', onWindowResize, false );
-
+  window.addEventListener("resize", onWindowResize, false);
 
   guiData = {
     drawFillShapes: true,
@@ -40,147 +38,117 @@ function init() {
     strokesWireframe: false
   };
 
-
-  loadSVG( '/cuff.svg' );
-
+  loadSVG("/cuff.svg");
 }
 
-function loadSVG( url ) {
-
+function loadSVG(url) {
   //
 
   scene = new THREE.Scene();
-  scene.background = new THREE.Color( 0xb0b0b0 );
+  scene.background = new THREE.Color(0xb0b0b0);
 
   //
 
-  var helper = new THREE.GridHelper( 160, 10 );
+  var helper = new THREE.GridHelper(160, 10);
   helper.rotation.x = Math.PI / 2;
-  scene.add( helper );
+  scene.add(helper);
 
   //
 
   var loader = new THREE.SVGLoader();
 
-  loader.load( url, function ( data ) {
-
+  loader.load(url, function(data) {
     var paths = data.paths;
 
-    
-
     var group = new THREE.Group();
-    group.scale.multiplyScalar( 20.25 );
+    group.scale.multiplyScalar(20.25);
     // group.position.x = - 70;
     // group.position.y = 70;
-    group.scale.y *= - 1;
+    group.scale.y *= -1;
 
-    for ( var i = 0; i < paths.length; i ++ ) {
 
-      var path = paths[ i ];
+    const leather = new THREE.ImageUtils.loadTexture("texture.jpg");
+    leather.wrapS = leather.wrapT = THREE.RepeatWrapping;
+    var uniformsL = {
+      leatherImage: { type: "t", value: leather }
+    };
 
-      var fillColor = path.userData.style.fill;
-      if ( guiData.drawFillShapes) {
+    var vs = document.getElementById("vertexShader").textContent;
+    var fsL = document.getElementById("fragmentShader-L").textContent;
 
-        var material = new THREE.MeshBasicMaterial( {
-          color: new THREE.Color().setStyle( fillColor ),
-          opacity: path.userData.style.fillOpacity,
-          transparent: path.userData.style.fillOpacity < 1,
-          side: THREE.DoubleSide,
-          depthWrite: false,
-          wireframe: guiData.fillShapesWireframe
-        } );
+    var material = new THREE.ShaderMaterial({
+      uniforms: uniformsL,
+      shading: THREE.FlatShading,
+      side: THREE.DoubleSide,
+      vertexShader: vs,
+      fragmentShader: fsL
+    });
 
-        var shapes = path.toShapes( false );
+    for (var i = 0; i < paths.length; i++) {
+      var path = paths[i];
 
-        for ( var j = 0; j < shapes.length; j ++ ) {
+      if (guiData.drawFillShapes) {
+        // var material = new THREE.MeshPhongMaterial({ color: 0x7777ff });
 
-          var shape = shapes[ j ];
+        // var material = new THREE.MeshBasicMaterial( {
+        //   color: new THREE.Color().setStyle( fillColor ),
+        //   opacity: path.userData.style.fillOpacity,
+        //   transparent: path.userData.style.fillOpacity < 1,
+        //   side: THREE.DoubleSide,
+        //   depthWrite: false,
+        //   wireframe: guiData.fillShapesWireframe
+        // } );
+
+        var shapes = path.toShapes(false);
+
+        for (var j = 0; j < shapes.length; j++) {
+          var shape = shapes[j];
 
           // var geometry = new THREE.ShapeBufferGeometry( shape );
           // var mesh = new THREE.Mesh( geometry, material );
 
           const depth = 0.1;
 
-          var shape3d = new THREE.ExtrudeBufferGeometry( shape, {
+          var shape3d = new THREE.ExtrudeBufferGeometry(shape, {
             depth: depth,
             bevelEnabled: false
-          } );
-  
+          });
 
-          var mesh = new THREE.Mesh( shape3d, material );
+          var mesh = new THREE.Mesh(shape3d, material);
           mesh.rotation.x = Math.PI;
-          mesh.translateZ( - depth - 1 );
-          // mesh.translateX( - center.x );
-          // mesh.translateY( - center.y );
+          mesh.translateZ(-depth - 1);
 
-          if (j === 0) {
-            console.log(shape)
-            group.add( mesh );
-          }
 
+          const center = new THREE.Vector3();
+          mesh.geometry.computeBoundingBox();
+          mesh.geometry.boundingBox.getCenter( center );
+          
+          mesh.translateX( - center.x );
+          mesh.translateY( - center.y );
+
+          console.log(shape);
+          group.add(mesh);
         }
-
       }
-
-      var strokeColor = path.userData.style.stroke;
-
-      if ( guiData.drawStrokes) {
-
-        var material = new THREE.MeshBasicMaterial( {
-          color: new THREE.Color().setStyle( strokeColor ),
-          opacity: path.userData.style.strokeOpacity,
-          transparent: path.userData.style.strokeOpacity < 1,
-          side: THREE.DoubleSide,
-          depthWrite: false,
-          wireframe: guiData.strokesWireframe
-        } );
-
-        for ( var j = 0, jl = path.subPaths.length; j < jl; j ++ ) {
-
-          var subPath = path.subPaths[ j ];
-          // console.log(subPath)
-
-          var geometry = THREE.SVGLoader.pointsToStroke( subPath.getPoints(), path.userData.style );
-
-          if ( geometry ) {
-
-            var mesh = new THREE.Mesh( geometry, material );
-
-            group.add( mesh );
-
-          }
-
-        }
-
-      }
-
     }
 
-    scene.add( group );
-
-  } );
-
+    scene.add(group);
+  });
 }
 
 function onWindowResize() {
-
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
 
-  renderer.setSize( window.innerWidth, window.innerHeight );
-
+  renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 function animate() {
-
-  requestAnimationFrame( animate );
+  requestAnimationFrame(animate);
 
   render();
-
 }
 
 function render() {
-
-  renderer.render( scene, camera );
-
+  renderer.render(scene, camera);
 }
